b1169781fec82f7a01015a49dc8bdc455a1ebeb9
Andrew Ohanian <ohanianandrew@gmail.com>
2022-08-15 09:05:26 -0500

    Added interface description to email body

--- send_email.py
+++ send_email.py
@@ -1,15 +1,25 @@
-from mailer import Mailer
-from mailer import Message
-from email.utils import formatdate
 import secrets
+import smtplib
+from email.message import EmailMessage
+
+def send_completed_email(router_hostname, router_port, show_run_output, telco_hostname):
+    sender = secrets.from_email
+    receivers = secrets.to_email_list
+    body = f'Please update the interface description for {router_hostname} {router_port} if necessary.\n\n\n'
+    body += 'Current interface description:\n\n'
+    body += show_run_output
+
+    msg = EmailMessage()
+    msg.set_content(body)
+
+    msg['Subject'] = f'{router_hostname} {router_port} successfully configured for {telco_hostname}'
+    msg['From'] = sender
+    msg['To'] = ', '.join(receivers)
 
 
-def send_completed_email(router_hostname, router_port, telco_hostname):
-    message      = Message(From=secrets.from_email, To=secrets.to_email_list)
-    message.Date = formatdate(localtime=True)
+    try:
+        smtpObj = smtplib.SMTP('localhost')
+        smtpObj.send_message(msg)
 
-    message.Subject = f'{router_hostname} {router_port} successfully configured for {telco_hostname}'
-    message.Body    = f'Please update the interface description for {router_hostname} {router_port}.'
-
-    sender = Mailer('localhost', 25)
-    sender.send(message)
+    except OSError:
+        print("Error: unable to send email")
